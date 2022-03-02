16c2aeccb6bcb03857ff9b0a62baa2cfbaf885fa
your_name <your_email>
2022-03-02 17:54:20 +0800

    fix

--- homework/hw2/rasterizer.cpp
+++ homework/hw2/rasterizer.cpp
@@ -156,7 +156,7 @@ void rst::rasterizer::rasterize_triangle(const Triangle& t, bool use_ssaa) {
                             float w_reciprocal = 1.0/(alpha / v[0].w() + beta / v[1].w() + gamma / v[2].w());
                             float z_interpolated = alpha * v[0].z() / v[0].w() + beta * v[1].z() / v[1].w() + gamma * v[2].z() / v[2].w();
                             z_interpolated *= w_reciprocal;
-                            int sample_point_index = get_index(x, y)*4 + s'sa'a_cnt;
+                            int sample_point_index = get_index(x, y)*4 + ssaa_cnt;
                             if (ssaa_depth_buf[sample_point_index] > z_interpolated) {
                                 ssaa_depth_buf[sample_point_index] = z_interpolated;
                                 ssaa_color_buf[sample_point_index] = t.getColor();
